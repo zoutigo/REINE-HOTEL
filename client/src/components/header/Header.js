99b988391da16291ef276067942587a0f946d8fe
zoutigo <zoutigo@gmail.com>
2020-11-23 16:17:55 +0100

    set header background

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import {AppBar, Toolbar} from '@material-ui/core'
+import {makeStyles} from '@material-ui/styles'
+
+const useStyles = makeStyles({
+    root: {
+        backgroundColor: '#0a0a0aff'
+    }
+})
 
 function Header() {
+    const classes = useStyles()
     return (
         <AppBar>
-            <Toolbar>
-                My Toolbar
+            <Toolbar className={classes.root}>
+                My header
             </Toolbar>
         </AppBar>
     )
