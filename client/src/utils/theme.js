18d7170dd51c1b7d78d960c446afb95e20e9c556
zoutigo <zoutigo@gmail.com>
2020-11-23 15:36:38 +0100

    define theme palette colors

--- client/src/utils/theme.js
+++ client/src/utils/theme.js
@@ -1,15 +1,35 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
-import {green, blue} from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
   palette: {
-    primary: {
-      main: purple[500],
+    dark: {
+      main: '#0a0a0aff',
     },
-    secondary: {
-      main: blue[500],
+    darkblue: {
+      main: 'rgba(10, 17, 31, 1)',
     },
+    blue :{
+      main: 'rgba(20, 33, 61, 1)',
+    },
+    coyotte :{
+      main: 'rgba(136, 98, 39, 1)',
+    },
+    orange :{
+      main: 'rgba(252, 163, 17, 1)',
+    },
+    gold :{
+      main: 'rgba(241, 196, 123, 1)',
+    },
+    wheat :{
+      main: 'rgba(235, 213, 176, 1)',
+    },
+    platinium :{
+      main: 'rgba(229, 229, 229, 1)',
+    },
+    white :{
+      main: 'rgba(255, 255, 255, 1)',
+    },
+    
   },
 });
 
